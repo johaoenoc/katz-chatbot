e0a3baa07191a6f8b339f5c1b820c0f945e2d06d
Joha <johao.alvarado@pedidosya.com>
2023-10-17 01:38:48 -0500

    autorespuestas
    autorespuestas con delay con exito

--- script.js
+++ script.js
@@ -66,41 +66,77 @@ function addMessage(user, message) {
 }
 
 function botResponse(userInput) {
-  
-  // Aquí puedes definir las respuestas del chatbot a diferentes entradas del usuario.
-  if (userInput.includes("hola")) {
-    return "¡Hola! ¿En qué puedo ayudarte?";
-  } else if (userInput.includes("ayuda")) {
-    return "Claro, ¿en qué puedo ayudarte?";
-  } else if (userInput.includes("gmail") || userInput.includes("correo")) {
-    return "Todo lo que necesites con respecto a Gmail lo hace el bot Alfred de Slack, para activarlo <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/como-usar-el-bot-alfred?pli=1&authuser=1'target='_blank'>este enlace</a>.";
-  } else if (userInput.includes("vpn") || userInput.includes("VPN")) {
-    return "Si tienes problemas con la VPN, recomendamos que primero verifiques la configuracion que esta en <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/acceso-a-vpn?authuser=1'target='_blank'> este enlace </a> ";
-  } else if (userInput.includes("okta") || userInput.includes("sso")) {
-    return "Todo lo que necesites con respecto a Okta SSO lo hace el bot Alfred de Slack, para activarlo <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/como-usar-el-bot-alfred?pli=1&authuser=1'target='_blank'>este enlace</a>.";
-  } else if (userInput.includes("pc") || userInput.includes("jumpcloud")) {
-    return "Si tienes problemas para acceder a la pc, tenemos estas guias para la autegestion desde Jumpcloud:<br><br><ul><li> <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/cambiar-contrase%C3%B1a-de-la-compu/jumpcloud-cambio-de-contrase%C3%B1a?pli=1&authuser=1' target='_blank'>Cambio de contraseña en Jumpcloud</a></li><br><li> <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/autenticaci%C3%B3n-en-2-pasos-jumpcloud?pli=1&authuser=1' target='_blank'>Activar Doble Autenticacion</a>.</li></ul>";
-  } 
-  else if (userInput.includes("katz") || userInput.includes("Katz")) {
-    return "<img src='Katz.png' width=80 alt='Descripción de la imagen'>"
-    
-  }
-    else {
-    return "Lo siento, no entiendo tu pregunta.";
+  // Convierte la entrada del usuario a minúsculas para que la búsqueda sea insensible a mayúsculas y minúsculas
+  const userInputLower = userInput.toLowerCase();
+
+  // Define las autorespuestas y sus respectivos retrasos en un objeto
+  const autorespuestas = [
+    {
+      keywords: ["hola", "HOLA", "HOLA!", "HOLA "],
+      respuesta: { mensaje: "¡Hola! ¿En qué puedo ayudarte?", delay: 3000 }
+    },
+    {
+      keywords: ["ayuda", "AYUDA"],
+      respuesta: { mensaje: "Claro, ¿en qué puedo ayudarte?", delay: 3000 }
+    },
+    {
+      keywords: ["gmail", "correo"],
+      respuesta: { mensaje: "Todo lo que necesites con respecto a Gmail lo hace el bot Alfred de Slack, para activarlo <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/como-usar-el-bot-alfred?pli=1&authuser=1'target='_blank'>este enlace</a>.", delay: 3000 }
+    },
+    {
+      keywords: ["vpn"],
+      respuesta: { mensaje: "Si tienes problemas con la VPN, recomendamos que primero verifiques la configuracion que esta en <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/acceso-a-vpn?authuser=1'target='_blank'> este enlace </a>.", delay: 3000 }
+    },
+    {
+      keywords: ["okta", "sso"],
+      respuesta: { mensaje: "Todo lo que necesites con respecto a Okta SSO lo hace el bot Alfred de Slack, para activarlo <a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/como-usar-el-bot-alfred?pli=1&authuser=1'target='_blank'>este enlace</a>.", delay: 3000 }
+    },
+    {
+      keywords: ["pc", "jumpcloud"],
+      respuesta: { mensaje: "Si tienes problemas para acceder a la pc, tenemos estas guias para la autegestion desde Jumpcloud:<br><br><ul><li><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/cambiar-contrase%C3%B1a-de-la-compu/jumpcloud-cambio-de-contrase%C3%B1a?pli=1&authuser=1' target='_blank'> Cambio de contraseña en Jumpcloud</a></li><br><li><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/autenticaci%C3%B3n-en-2-pasos-jumpcloud?pli=1&authuser=1' target='_blank'> Activar Doble Autenticacion</a>.</li></ul>", delay: 3000 }
+    },
+    {
+      keywords: ["katz"],
+      respuesta: { mensaje: "<img src='Katz.png' width=80 alt='Descripción de la imagen'>", delay: 3000 }
+    }
+  ];
+
+  // Verifica si hay una autorespuesta definida para la entrada del usuario
+  for (const respuesta of autorespuestas) {
+    for (const keyword of respuesta.keywords) {
+      if (userInputLower.includes(keyword)) {
+        // Agrega un retraso antes de mostrar la autorespuesta
+        setTimeout(() => {
+          addMessage("bot", respuesta.respuesta.mensaje);
+        }, respuesta.respuesta.delay);
+
+        return;
+      }
+    }
   }
 
+  // Si no hay una autorespuesta definida, muestra la respuesta predeterminada sin retraso
+  addMessage("bot", "Lo siento, no entiendo tu pregunta.");
 }
 
-sendButton.addEventListener("click", () => {
-  const userMessage = userInput.value;
-  addMessage("user", userMessage);
-  userInput.value = "";
 
-  const botMessage = botResponse(userMessage);
-  addMessage("bot", botMessage);
-  const sendSound = document.getElementById("sendSound");
-        sendSound.play();
+
+
+
+
+sendButton.addEventListener("click", () => {
+  const userMessage = userInput.value.trim(); // Trim para eliminar espacios en blanco al inicio y al final
+  if (userMessage) { // Verificar si el mensaje del usuario no está vacío
+    addMessage("user", userMessage);
+    userInput.value = "";
+
+    const sendSound = document.getElementById("sendSound");
+    sendSound.play();
+
+    botResponse(userMessage); // Llama a botResponse sin esperar una respuesta específica
+  }
 });
+
 
 userInput.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
