142036c8e258f847b61c0b52801857dd91c45e60
Joha <johao.alvarado@pedidosya.com>
2023-11-22 17:52:07 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -183,8 +183,10 @@ function botResponse(userInput) {
     },
     {
       keywords: ["jira"],
-      respuesta: { mensaje: "<p>Tenemos las siguientes guías para Jira Service Management:</p><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/agregar-personas-a-proyectos?authuser=0' target='_blank'>Agregar personas a un proyecto</a><br><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/agregar-organizaciones-a-un-proyecto?authuser=0' target='_blank'>Agregar organizaciones a proyectos</a><br><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/crear-nueva-organizaci%C3%B3n?authuser=0' target='_blank'>Crear nueva organización</a><br><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/agregar-organizaciones-a-un-proyecto?authuser=0' target='_blank'>Agregar cliente a organización</a>"
-      , delay: 3000 },
+      respuesta: {
+        mensaje: "<p>Tenemos las siguientes guías para Jira Service Management:</p><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/agregar-personas-a-proyectos?authuser=0' target='_blank'>Agregar personas a un proyecto</a><br><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/agregar-organizaciones-a-un-proyecto?authuser=0' target='_blank'>Agregar organizaciones a proyectos</a><br><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/crear-nueva-organizaci%C3%B3n?authuser=0' target='_blank'>Crear nueva organización</a><br><br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/jira-service-management/agregar-organizaciones-a-un-proyecto?authuser=0' target='_blank'>Agregar cliente a organización</a>"
+        , delay: 3000
+      },
     },
     {
       keywords: ["tableau", "salesforce", "backoffice", "looker", "monday", "datadog", "bitly", "adjust", "ariba", "braze", "sap", "thinkcell", "adobe", "links de accesos"],
@@ -194,7 +196,7 @@ function botResponse(userInput) {
       keywords: ["office", "microsoft", "licencia de office"],
       respuesta: { mensaje: "¿Cómo activar mi licencia de Office? <br><br> Para activarla te dejo la guía en <br><a href='https://sites.google.com/pedidosya.com/itsupport/instructivos/otros-manuales/microsoft-office' target='_blank'>este enlace</a>.", delay: 3000 },
     }
-    
+
   ];
 
   // Verifica si hay una autorespuesta definida para la entrada del usuario
